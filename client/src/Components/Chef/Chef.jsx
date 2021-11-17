2daf89a48a476e2a77fa80a75e6cbe0bb00e5129
pranav131100 <pranav.shirke1311@gmail.com>
2021-11-17 09:33:06 +0530

    content changed

--- client/src/Components/Chef/Chef.jsx
+++ client/src/Components/Chef/Chef.jsx
@@ -4,25 +4,27 @@ import React from 'react'
 import team3 from './team-3.jpg'
 const Chef = () => {
     return (
-        <div>
-            <div className="col-lg-3 col-md-6">
-                <div className="team-item">
-                    <div className="team-img">
-                        <img src={team3} alt="Image" />
-                        <div className="team-social">
-                            <a href=""><i className="fab fa-twitter"></i></a>
-                            <a href=""><i className="fab fa-facebook-f"></i></a>
-                            <a href=""><i className="fab fa-linkedin-in"></i></a>
-                            <a href=""><i className="fab fa-instagram"></i></a>
-                        </div>
-                    </div>
-                    <div className="team-text">
-                        <h2>Adam Phillips</h2>
-                        <p>CEO, Co Founder</p>
-                    </div>
+        <>
+
+<div className="col-lg-3 col-md-6">
+              <div className="team-item">
+                <div className="team-img">
+                  <img src="img/team-1.jpg" alt="Image" />
+                  <div className="team-social">
+                    <a href><i className="fab fa-twitter" /></a>
+                    <a href><i className="fab fa-facebook-f" /></a>
+                    <a href><i className="fab fa-linkedin-in" /></a>
+                    <a href><i className="fab fa-instagram" /></a>
+                  </div>
                 </div>
+                <div className="team-text">
+                  <h2>Adam Phillips</h2>
+                  <p>CEO, Co Founder</p>
+                </div>
+              </div>
             </div>
-        </div>
+
+        </>
     )
 }
 
